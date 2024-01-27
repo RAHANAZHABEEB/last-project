d0bc1cbc2bc6305eaea13caeb4058e0e63e82984
rahanaz <rahanazhabeeb2001@gmail.com>
2024-01-27 03:51:46 -0800

    add

--- pro1/app1/urls.py
+++ pro1/app1/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('details/<int:id>/', views.details, name="details"),
     path('chick/',views.chick,name='chick'),
     path('about/',views.about,name="about"),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
